3c965ba9429c2518bac20949f829e42b6f3a4727
Yuji181181 <s1f102402552@iniad.org>
2025-09-13 00:20:58 +0900

    create-readAPI

--- src/app/api/survey/[id]/route.ts
+++ src/app/api/survey/[id]/route.ts
@@ -3,9 +3,44 @@ import {
   updateSurveyRequestSchema,
 } from "@/schemas/api/survey";
 import type { ResBody } from "@/types/api";
-import type { UpdateSurveyResponse } from "@/types/api/survey";
+import type { Survey, UpdateSurveyResponse } from "@/types/api/survey";
 import { NextResponse } from "next/server";
+import { getSurveyById } from "../../(Repository)/read";
 import { updateSurvey } from "../../(Repository)/update";
+
+export const GET = async (
+  _request: Request, // requestは認証で使うかもなので_request
+  { params }: { params: Promise<{ id: string }> },
+) => {
+  try {
+    // paramsはPromiseなのでawaitで解決
+    const resolvedParams = await params;
+    // パラメータのバリデーション
+    const validatedParams = surveyParamsSchema.parse(resolvedParams);
+
+    // 投稿詳細を取得
+    const survey = await getSurveyById(validatedParams.id);
+
+    if (!survey) {
+      return NextResponse.json<ResBody<undefined>>(
+        { message: "Survey not found" },
+        { status: 404 },
+      );
+    }
+
+    return NextResponse.json<ResBody<Survey>>(
+      { message: "Success", data: survey },
+      { status: 200 },
+    );
+  } catch (error) {
+    console.error(error);
+
+    return NextResponse.json<ResBody<undefined>>(
+      { message: "Internal Server Error" },
+      { status: 500 },
+    );
+  }
+};
 
 export const PUT = async (
   request: Request,
