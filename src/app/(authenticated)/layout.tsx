3ec647cbcf46b4724741705ad0f40f4708785f21
Yuji181181 <s1f102402552@iniad.org>
2025-10-19 19:10:42 +0900

    fix:回答確認ページでヘッダーとカードが被る問題を修正

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -10,7 +10,7 @@ export default function AuthenticatedLayout({
   return (
     <div className="flex min-h-screen flex-col bg-background">
       <Header />
-      <div className="flex-1">{children}</div>
+      <div className="flex-1 pt-4">{children}</div>
       <FabCreateButton />
     </div>
   );
